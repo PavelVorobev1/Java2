5aa49da7aa9dfa435373581f8eb64d43b134559e
Pavel <Frozzty@yandex.ru>
2021-12-23 20:12:51 +0300

    Practical task for lesson 2.

---
+++ src/lesson2/MyArraySizeException.java
@@ -0,0 +1,8 @@
+package lesson2;
+
+public class MyArraySizeException extends RuntimeException{
+
+    public MyArraySizeException() {
+        super("Колчество строк и столбцов не должно быть больше 4.");
+    }
+}
