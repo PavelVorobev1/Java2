b94983c6b558d4b0960d7817b9a93ce87da58f1f
Pavel <91744613+PavelKazAn@users.noreply.github.com>
2021-12-26 14:13:49 +0200

    Delete MyArraySizeException.java

--- src/lesson2/MyArraySizeException.java
+++
@@ -1,8 +0,0 @@
-package lesson2;
-
-public class MyArraySizeException extends RuntimeException{
-
-    public MyArraySizeException() {
-        super("Колчество строк и столбцов не должно быть больше 4.");
-    }
-}
