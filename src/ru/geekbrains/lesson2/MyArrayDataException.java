7cebac875c383334881d159516910c2194b42937
Pavel <91744613+PavelKazAn@users.noreply.github.com>
2022-01-14 19:36:05 +0200

    Delete MyArrayDataException.java

--- src/ru/geekbrains/lesson2/MyArrayDataException.java
+++
@@ -1,8 +0,0 @@
-package ru.geekbrains.lesson2;
-
-public class MyArrayDataException extends RuntimeException {
-
-    public MyArrayDataException() {
-        super("Неверный формат ввода чисел.");
-    }
-}
